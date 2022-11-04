13138fa0dee6eaab0095ba88d15be85e7f7f4c11
adeelahmed057 <adeelnazir2018@gmail.com>
2022-11-04 15:34:49 +0500

    4th day

--- src/components/AllPool/CandleChart.jsx
+++ src/components/AllPool/CandleChart.jsx
@@ -1,62 +1,77 @@
 import React from 'react';
 import Chart from "react-google-charts";
+// import './AllPool.css'
 
 
 function CandleChart() {
-    
+  const options = {
+    chart: {},
+    width: '100%',
+    // height: 300,
+    // backgroundColor: '#4A5568',
+    // color:"white",
+    hAxis: { title: '' },
 
-const data = [
-  [
-    {
-      type: "string",
-      id: "Date"
-    },
-    {
-      type: "number",
-      label: "Something"
-    },
-    {
-      type: "number",
-      label: "Something"
-    },
-    {
-      type: "number",
-      label: "Something"
-    },
-    {
-      type: "number",
-      label: "Something"
-    }
-  ],
-  ["Mon", 20, 28, 38, 45],
-  ["Tue", 31, 38, 55, 66],
-  ["Wed", 50, 55, 77, 80],
-  ["Thu", 77, 77, 66, 50],
-  ["Mon", 20, 28, 38, 45],
-  ["Tue", 31, 38, 55, 66],
-  ["Wed", 50, 55, 77, 80],
-  ["Thu", 77, 77, 66, 50],
-  ["Mon", 20, 28, 38, 45],
-  ["Tue", 31, 38, 55, 66],
-  ["Wed", 50, 55, 77, 80],
-  ["Thu", 77, 77, 66, 50],
-  ["Mon", 20, 28, 38, 45],
-  ["Tue", 31, 38, 55, 66],
-  ["Wed", 50, 55, 77, 80],
-  ["Thu", 77, 77, 66, 50],
-  ["Fri", 68, 66, 22, 15]
-];
-    return (
-        <div className=''>
-        <div className="App">
-          <Chart
-            chartType="CandlestickChart"
-            height="500px"
-            data={data}
-          />
-        </div>
-        </div>
-      );
+  };
+
+
+  const data = [
+    [
+      {
+        type: "string",
+        id: "Date"
+      },
+      {
+        type: "number",
+        label: ""
+      },
+      {
+        type: "number",
+        label: ""
+      },
+      {
+        type: "number",
+        label: ""
+      },
+      {
+        type: "number",
+        label: ""
+      }
+    ],
+    ["Mon", 20, 28, 38, 45],
+    ["Tue", 31, 38, 55, 66],
+    ["Wed", 50, 55, 77, 80],
+    ["Thu", 77, 77, 66, 50],
+    ["Mon", 20, 28, 38, 45],
+    ["Tue", 31, 38, 55, 66],
+    ["Wed", 50, 55, 77, 80],
+    ["Thu", 77, 77, 66, 50],
+    ["Mon", 20, 28, 38, 45],
+    ["Tue", 31, 38, 55, 66],
+    ["Wed", 50, 55, 77, 80],
+    ["Thu", 77, 77, 66, 50],
+    ["Mon", 20, 28, 38, 45],
+    ["Tue", 31, 38, 55, 66],
+    ["Wed", 50, 55, 77, 80],
+    ["Thu", 77, 77, 66, 50],
+    ["Fri", 68, 66, 22, 15]
+  ];
+  return (
+    <div className='bg-App'>
+      <div className="bg-App">
+        <Chart
+          // id="reactgooglegraph-1 Chart_div"
+          className='bg-App'
+          chartType="CandlestickChart"
+          height="450px"
+          fill="#000000"
+          data={data}
+          options={options}
+          
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CandleChart
--- src/components/AllPool/DetailPage.jsx
+++ src/components/AllPool/DetailPage.jsx
@@ -5,33 +5,33 @@ import { Link } from 'react-router-dom';
 import CandleChart from './CandleChart';
 import { Button } from 'rebass';
 function DetailPage(props) {
-const web3=new Web3("https://bsc-dataseed1.binance.org/");
-const [hours, setHours]=useState(true);
-const [days, setDays]=useState(false);
-const setHoursFunc=()=>{
-    setHours(true)
-    setDays(false)
-}
-const setDaysFunc=()=>{
-    setHours(false)
-    setDays(true)
-}
+    const web3 = new Web3("https://bsc-dataseed1.binance.org/");
+    const [hours, setHours] = useState(true);
+    const [days, setDays] = useState(false);
+    const setHoursFunc = () => {
+        setHours(true)
+        setDays(false)
+    }
+    const setDaysFunc = () => {
+        setHours(false)
+        setDays(true)
+    }
     return (
         <>
             <div className='container'>
                 <div className='col-md-12 text-white'>
                     <div className="d-flex justify-content-end m-4">
-                        <a  href={`https://bscscan.com/address/${props.dataProps.address}#code`} target='_blank'>View on BscScan &nbsp;<i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                        <a href={`https://bscscan.com/address/${props?.dataProps?.address}#code`} target='_blank'>View on BscScan &nbsp;<i class="fa-solid fa-arrow-up-right-from-square"></i></a>
                     </div>
                 </div>
                 <div className="col-md-12 text-white D-text">
-                    <i class="fa-regular fa-circle-question"></i> <i class="fa-regular fa-circle-question"></i> &nbsp; {`${props.dataProps.symbol1}/${props.dataProps.symbol2}`}
+                    <i class="fa-regular fa-circle-question"></i> <i class="fa-regular fa-circle-question"></i> &nbsp; {`${props?.dataProps?.symbol1}/${props.dataProps.symbol2}`}
                 </div>
                 <div className='row text-white mt-3'>
                     <div className='col-md-8'>
                         <div className="d-flex flex-row mt-3 align-self-center bd-high">
-                            <div className="p-2 bd-highlight"><i class="fa-regular fa-circle-question"></i> &nbsp; {props.dataProps.symbol1 =="WBNB" ? `1 ${props.dataProps.symbol2}=${(props.dataProps.reserve0 / props.dataProps.reserve1).toFixed(7)}   ${props.dataProps.symbol1}`:`1 ${props.dataProps.symbol1}=${(props.dataProps.reserve1 / props.dataProps.reserve0).toFixed(7)}  ${props.dataProps.symbol2}`} </div>
-                            <div className="p-2 bd-highlight"><i class="fa-regular fa-circle-question"></i> &nbsp; {props.dataProps.symbol1 =="WBNB" ? `1 ${props.dataProps.symbol1}=${(props.dataProps.reserve1 / props.dataProps.reserve0).toFixed(7)}  ${props.dataProps.symbol2}`:`1 ${props.dataProps.symbol2}=${(props.dataProps.reserve0 / props.dataProps.reserve1).toFixed(7)}  ${props.dataProps.symbol1}`} </div>
+                            <div className="p-2 bd-highlight"><i class="fa-regular fa-circle-question"></i> &nbsp; {props?.dataProps?.symbol1 == "WBNB" ? `1 ${props.dataProps.symbol2}=${(props.dataProps.reserve0 / props.dataProps.reserve1).toFixed(7)}   ${props.dataProps.symbol1}` : `1 ${props.dataProps.symbol1}=${(props.dataProps.reserve1 / props.dataProps.reserve0).toFixed(7)}  ${props.dataProps.symbol2}`} </div>
+                            <div className="p-2 bd-highlight"><i class="fa-regular fa-circle-question"></i> &nbsp; {props?.dataProps?.symbol1 == "WBNB" ? `1 ${props.dataProps.symbol1}=${(props.dataProps.reserve1 / props.dataProps.reserve0).toFixed(7)}  ${props.dataProps.symbol2}` : `1 ${props.dataProps.symbol2}=${(props.dataProps.reserve0 / props.dataProps.reserve1).toFixed(7)}  ${props.dataProps.symbol1}`} </div>
                         </div>
                     </div>
                     <div className='col-md-4'>
@@ -44,51 +44,57 @@ const setDaysFunc=()=>{
 
                 <div className='row'>
                     <div className='col-md-4 '>
-                    <div className='text-white box p-2'>
-                        <div className='m-2'>
-                            <h4>TOTAL TOKENS LOCKED</h4>
-                        </div>
-                        <div className="box-1">
-                            <div className="d-flex flex-row  align-self-center  justify-content-between">
-                                <div className="p-2"><i class="fa-regular fa-circle-question"></i> &nbsp;{props.dataProps.symbol1}</div>
-                                <div className="p-2">{parseFloat(web3.utils.fromWei(props.dataProps.reserve0)).toFixed(6)}</div>
+                        <div className='text-white box p-2'>
+                            <div className='m-2'>
+                                <h4>TOTAL TOKENS LOCKED</h4>
                             </div>
-                            <div className="d-flex flex-row  align-self-center justify-content-between">
-                                <div className=" p-2"><i class="fa-regular fa-circle-question"></i> &nbsp;{props.dataProps.symbol2}</div>
-                                <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve1)).toFixed(6)}</div>
+                            <div className="box-1">
+                                <div className="d-flex flex-row  align-self-center  justify-content-between">
+                                    <div className="p-2"><i class="fa-regular fa-circle-question"></i> &nbsp;{props?.dataProps?.symbol1}</div>
+                                    <div className="p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve0)).toFixed(6)}</div>
+                                </div>
+                                <div className="d-flex flex-row  align-self-center justify-content-between">
+                                    <div className=" p-2"><i class="fa-regular fa-circle-question"></i> &nbsp;{props?.dataProps?.symbol2}</div>
+                                    <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve1)).toFixed(6)}</div>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className='m-2'>
-                            <h4 className=''>Volume</h4>
-                        </div>
-                        <div className="box-2">
-                            <div className="d-flex flex-row  align-items-center bd-highlight justify-content-between">
-                                <div className="p-2 mt-2 bd-highlight"><i class="fa-regular fa-circle-question"></i></div>
-                                <div className="p-2  mt-2 bd-highlight">0000000K</div>
+                            <div className='m-2'>
+                                <h4 className=''>Volume</h4>
                             </div>
-                        </div>
+                            <div className="box-2">
+                                <div className="d-flex flex-row  align-items-center bd-highlight justify-content-between">
+                                    <div className="p-2 mt-2 bd-highlight"><i class="fa-regular fa-circle-question"></i></div>
+                                    <div className="p-2  mt-2 bd-highlight">0000000K</div>
+                                </div>
+                            </div>
                         </div>
                         <div className='text-white box p-2'>
-                        <div className='m-2 d-flex justify-content-between'>
-                   
-                             <button type='button' className={` btn-lg  ${hours==true?"btn":"btn-dis"}`} onClick={()=>setHoursFunc()}> Last 24 Hours</button> 
-                              <button type='button' className={` btn-lg  ${days==true?"btn":"btn-dis"}`} onClick={()=>setDaysFunc()} >7Days</button> 
-                     
-                           
-                        </div>
-                        <div className="box-1">
-                            <div className="d-flex flex-row  align-self-center  justify-content-between">
-                                <div className="p-2"><i class="fa-regular fa-circle-question"></i> &nbsp;Volume 24h</div>
-                                <div className="p-2">LP REWARD FEES 24H</div>
-                            </div>
-                            <div className="d-flex flex-row  align-self-center justify-content-center">
-                                {/* <div className=" p-2"><i class="fa-regular fa-circle-question"></i>{parseFloat(web3.utils.fromWei(props.dataProps.reserve0)).toFixed(6)}</div> */}
-                                <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve1)).toFixed(6)}</div>
-                            </div>
-                        </div>
+                            <div className='m-2 d-flex align-items-center justify-content-center'>
+                                <div className=" d-grid gap-3 ">
+                                    <button className={`${hours == true ? "btn btn-lg" : "btn-dis"}`} onClick={() => setHoursFunc()} type="button">Last 24 Hours</button>
+                                    <button className={`${days == true ? "btn btn-block " : "btn-dis"}`} onClick={() => setDaysFunc()} type="button">7days</button>
+                                </div>
+                                {/* <div className='btn btn-block'><button type='button' className={`${hours == true ? "btn" : "btn-dis"}`} onClick={() => setHoursFunc()}> Last 24 Hours</button></div>
+                                <div className='btn btn-block'> <button type='button' className={`  ${days == true ? "btn" : "btn-dis"}`} onClick={() => setDaysFunc()} >7Days</button> </div> */}
 
-                        <div className='m-2'>
+                            </div>
+                            <div className="mt-3 box-1">
+                                <div className="d-flex flex-row  align-self-center  justify-content-between">
+                                    <div className="p-2"><i class="fa-regular fa-circle-question"></i> &nbsp;Volume 24h</div>
+                                    <div className="p-2">LP REWARD FEES 24H</div>
+                                </div>
+                                <div className="d-flex flex-row  align-self-center justify-content-between">
+                                    <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve0)).toFixed(6)}</div>
+                                    <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve1)).toFixed(6)}</div>
+                                </div>
+                                <div className="d-flex flex-row  align-self-center justify-content-between">
+                                    <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve0)).toFixed(6)}</div>
+                                    <div className=" p-2">{parseFloat(web3.utils.fromWei(props?.dataProps?.reserve1)).toFixed(6)}</div>
+                                </div>
+                            </div>
+
+                            {/* <div className='m-2'>
                             <h4 className=''>Volume</h4>
                         </div>
                         <div className="box-2">
@@ -96,12 +102,12 @@ const setDaysFunc=()=>{
                                 <div className="p-2 mt-2 bd-highlight"><i class="fa-regular fa-circle-question"></i></div>
                                 <div className="p-2  mt-2 bd-highlight">0000000K</div>
                             </div>
+                        </div> */}
                         </div>
-                    </div>
                     </div>
 
                     <div className='col-md-8 mt-4'>
-                    <CandleChart/>
+                        <CandleChart />
                     </div>
                 </div>
                 {/* <div className='row d-flex justify-content-center'>
@@ -123,10 +129,10 @@ const setDaysFunc=()=>{
                     </div>
                 </div> */}
                 <div className='row d-flex'>
-                
+
                 </div>
             </div>
-            
+
 
         </>
     )
